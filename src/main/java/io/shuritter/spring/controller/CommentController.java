9c2b2dcdaed75eff05dc5a53029047c206b7740d
saintshura <alekshurik.nyrkov@gmail.com>
2017-05-16 04:17:28 +0300

    add new methods

--- src/main/java/io/shuritter/spring/controller/CommentController.java
+++ src/main/java/io/shuritter/spring/controller/CommentController.java
@@ -1,14 +1,15 @@
 package io.shuritter.spring.controller;
 
 import io.shuritter.spring.model.Comment;
+import io.shuritter.spring.model.response.Response;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface CommentController extends BaseController<Comment> {
-    ResponseEntity<List<Comment>> getAll(String userId, String postId);
+    ResponseEntity<Response> getAll(String userId, String postId);
     ResponseEntity<Comment> add(String userId, String postId, Comment comment);
     ResponseEntity<Comment> update(String userId, String postId, String id, Comment comment);
     ResponseEntity<Comment> delete(String userId, String postId, String id, Comment comment);
-    ResponseEntity<Comment> getById(String userId, String postId, String id);
+    ResponseEntity<Response> getById(String userId, String postId, String id);
 }
--- src/main/java/io/shuritter/spring/controller/PostController.java
+++ src/main/java/io/shuritter/spring/controller/PostController.java
@@ -1,10 +1,13 @@
 package io.shuritter.spring.controller;
 
 import io.shuritter.spring.model.Post;
+import io.shuritter.spring.model.response.Response;
 import org.springframework.http.ResponseEntity;
 
 public interface PostController extends BaseController<Post> {
     ResponseEntity<Post> add(String id, Post post);
     ResponseEntity<Post> update(String userId, String id, Post post);
     ResponseEntity<Post> delete(String userId, String id);
+    ResponseEntity<Response> getAll(String userId);
+    ResponseEntity<Response> getById(String userId, String id);
 }
