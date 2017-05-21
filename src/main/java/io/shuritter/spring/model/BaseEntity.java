f4d18ef6563aebb5c944a3d377d3e6ba8c516dcb
saintshura <alekshurik.nyrkov@gmail.com>
2017-05-21 04:12:37 +0300

    Transferred repetitive fields from entity classes and used Lombok

--- src/main/java/io/shuritter/spring/model/BaseEntity.java
+++ src/main/java/io/shuritter/spring/model/BaseEntity.java
@@ -1,45 +1,67 @@
 package io.shuritter.spring.model;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.joda.ser.DateTimeSerializer;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME;
 
 @MappedSuperclass
+@EqualsAndHashCode
 public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "ID")
+    @Getter @Setter
     protected String id;
 
-    public String getId() {
-        return id;
+
+    @Column(name = "CREATED_AT")
+    @DateTimeFormat(iso = DATE_TIME)
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+    @JsonSerialize(using=DateTimeSerializer.class)
+    @Getter
+    protected DateTime createdAt;
+
+
+    @Column(name = "UPDATED_AT")
+    @DateTimeFormat(iso = DATE_TIME)
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
+    @JsonSerialize(using=DateTimeSerializer.class)
+    @Getter @Setter
+    protected DateTime updatedAt;
+
+    @Column(name = "IS_DELETED")
+    @Getter @Setter
+    protected Boolean isDeleted;
+
+    public BaseEntity() {
+        this.createdAt = DateTime.now();
+        this.updatedAt = DateTime.now();
+        this.isDeleted = false;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        BaseEntity that = (BaseEntity) o;
-
-        return id != null ? id.equals(that.id) : that.id == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "BaseEntity{" +
-                "id='" + id + '\'' +
-                '}';
+    public BaseEntity(Boolean isDeleted) {
+        this.createdAt = DateTime.now();
+        this.updatedAt = DateTime.now();
+        this.isDeleted = isDeleted;
     }
 }
