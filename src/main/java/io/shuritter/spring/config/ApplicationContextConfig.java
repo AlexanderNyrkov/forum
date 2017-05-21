9eda7dfe56a9e885aa50706345d67ce2a9e2f9f5
saintshura <alekshurik.nyrkov@gmail.com>
2017-05-21 04:08:27 +0300

    Created configuration without xml

--- src/main/java/io/shuritter/spring/config/ApplicationContextConfig.java
+++ src/main/java/io/shuritter/spring/config/ApplicationContextConfig.java
@@ -1,67 +1,66 @@
-package io.shuritter.spring.config;//package io.shuritter.spring.config;
-//
-//import org.apache.commons.dbcp2.BasicDataSource;
-//import org.hibernate.SessionFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.orm.hibernate5.HibernateTransactionManager;
-//import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.view.InternalResourceViewResolver;
-//
-//import javax.sql.DataSource;
-//import java.util.Properties;
-//
-//@Configuration
-//@EnableWebMvc
-//@ComponentScan("io.shuritter.spring")
-//@EnableTransactionManagement
-//public class ApplicationContextConfig {
-//    @Bean(name = "viewResolver")
-//    public InternalResourceViewResolver getViewResolver() {
-//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//        viewResolver.setPrefix("/WEB-INF/views/");
-//        viewResolver.setSuffix(".jsp");
-//        return viewResolver;
-//    }
-//
-//    @Bean(name = "dataSource")
-//    public DataSource getDataSource() {
-//        BasicDataSource dataSource = new BasicDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl("jdbc:postgresql://localhost:5432/shuritter");
-//
-//        return dataSource;
-//    }
-//
-//    @Autowired
-//    @Bean(name = "sessionFactory")
-//    public SessionFactory getSessionFactory(DataSource dataSource) {
-//
-//        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-//        sessionBuilder.addProperties(getHibernateProperties());
-//        sessionBuilder.scanPackages("io.shuritter.spring");
-//
-//        return sessionBuilder.buildSessionFactory();
-//    }
-//
-//    private Properties getHibernateProperties() {
-//        Properties properties = new Properties();
-//        properties.put("hibernate.show_sql", "true");
-//        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
-//        return properties;
-//    }
-//
-//    @Autowired
-//    @Bean(name = "transactionManager")
-//    public HibernateTransactionManager getTransactionManager(
-//            SessionFactory sessionFactory) {
-//        HibernateTransactionManager transactionManager = new HibernateTransactionManager(
-//                sessionFactory);
-//
-//        return transactionManager;
-//    }
-//}
+package io.shuritter.spring.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("io.shuritter.spring")
+@EnableTransactionManagement
+public class ApplicationContextConfig {
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/shuritter");
+
+        return dataSource;
+    }
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) {
+
+        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+        sessionBuilder.addProperties(getHibernateProperties());
+        sessionBuilder.scanPackages("io.shuritter.spring");
+
+        return sessionBuilder.buildSessionFactory();
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
+        return properties;
+    }
+
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(
+            SessionFactory sessionFactory) {
+
+        return new HibernateTransactionManager(
+                sessionFactory);
+    }
+}
--- src/main/java/io/shuritter/spring/config/SpringWebAppInitializer.java
+++ src/main/java/io/shuritter/spring/config/SpringWebAppInitializer.java
@@ -1,35 +1,31 @@
-package io.shuritter.spring.config;//package io.shuritter.spring.config;
-//
-//import io.shuritter.spring.config.ApplicationContextConfig;
-//import org.springframework.web.WebApplicationInitializer;
-//import org.springframework.web.context.ContextLoaderListener;
-//import org.springframework.web.context.WebApplicationContext;
-//import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-//import org.springframework.web.servlet.DispatcherServlet;
-//
-//import javax.servlet.ServletContext;
-//import javax.servlet.ServletException;
-//import javax.servlet.ServletRegistration;
-//
-///**
-// * Created by saintshura on 08/05/17.
-// */
-//public class SpringWebAppInitializer implements WebApplicationInitializer{
-//    @Override
-//    public void onStartup(ServletContext servletContext) throws ServletException {
-//        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-//        appContext.register(ApplicationContextConfig.class);
-//        WebApplicationContext context = getContext();
-//        servletContext.addListener(new ContextLoaderListener(context));
-//        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
-//                "SpringDispatcher", new DispatcherServlet(appContext));
-//        dispatcher.setLoadOnStartup(1);
-//        dispatcher.addMapping("/*");
-//    }
-//
-//    private AnnotationConfigWebApplicationContext getContext() {
-//        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-//        context.setConfigLocation("io.shuritter.spring.config");
-//        return context;
-//    }
-//}
+package io.shuritter.spring.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class SpringWebAppInitializer implements WebApplicationInitializer{
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(ApplicationContextConfig.class);
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
+                "SpringDispatcher", new DispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/*");
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("io.shuritter.spring.config");
+        return context;
+    }
+}
