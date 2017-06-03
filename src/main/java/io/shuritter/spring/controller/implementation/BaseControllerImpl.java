ed111d34c305272f73dfdccc09b3e72416e49a07
saintshura <alekshurik.nyrkov@gmail.com>
2017-06-03 04:14:19 +0300

    Added methods for verifying authorization, matching username and password with those sent at authorization

--- src/main/java/io/shuritter/spring/controller/implementation/BaseControllerImpl.java
+++ src/main/java/io/shuritter/spring/controller/implementation/BaseControllerImpl.java
@@ -2,6 +2,12 @@ package io.shuritter.spring.controller.implementation;
 
 import io.shuritter.spring.controller.BaseController;
 import io.shuritter.spring.model.BaseEntity;
+import io.shuritter.spring.model.User;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 /**
  * Abstract base class for Controller classes
@@ -11,4 +17,17 @@ import io.shuritter.spring.model.BaseEntity;
  */
 public abstract class BaseControllerImpl<T extends BaseEntity> implements BaseController<T> {
 
+    public Boolean authorization(HttpServletRequest request) {
+        final String authorization = request.getHeader("Authorization");
+        return authorization == null || !authorization.startsWith("Basic");
+    }
+
+    public Boolean loginPassword(HttpServletRequest request, User user) {
+        final String authorization = request.getHeader("Authorization");
+        String base64Credentials = authorization.substring("Basic".length()).trim();
+        String credentials = new String(Base64.getDecoder().decode(base64Credentials),
+                Charset.forName("UTF-8"));
+        final String[] values = credentials.split(":");
+        return !values[0].equals(user.getLogin()) || !BCrypt.checkpw(values[1], user.getPassword());
+    }
 }
