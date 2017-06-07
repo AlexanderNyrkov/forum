71287cbd7b678da1cb8b414346859643b1fc4c74
saintshura <alekshurik.nyrkov@gmail.com>
2017-06-07 07:06:19 +0300

    Correct comments

--- src/main/java/io/shuritter/spring/config/ApplicationContextConfig.java
+++ src/main/java/io/shuritter/spring/config/ApplicationContextConfig.java
@@ -2,12 +2,13 @@ package io.shuritter.spring.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -20,10 +21,14 @@ import java.util.Properties;
  */
 @Configuration
 @EnableWebMvc
+@EnableWebSecurity
 @ComponentScan("io.shuritter.spring")
 @EnableTransactionManagement
-public class ApplicationContextConfig {
+public class ApplicationContextConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * @return basic configured via JavaBeans properties
+     */
     @Bean(name = "dataSource")
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
@@ -33,30 +38,30 @@ public class ApplicationContextConfig {
         return dataSource;
     }
 
-    @Autowired
+    /**
+     * Create a SessionFactory using the properties and mappings in this configuration
+     * @param dataSource basic configured via JavaBeans properties
+     * @return the build SessionFactory
+     */
     @Bean(name = "sessionFactory")
     public SessionFactory getSessionFactory(DataSource dataSource) {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
 
         LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-        sessionBuilder.addProperties(getHibernateProperties());
+        sessionBuilder.addProperties(properties);
         sessionBuilder.scanPackages("io.shuritter.spring");
 
         return sessionBuilder.buildSessionFactory();
     }
 
-    private Properties getHibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.show_sql", "true");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL95Dialect");
-        return properties;
-    }
-
-    @Autowired
+    /**
+     * @param sessionFactory to manage transactions
+     * @return new HibernateTransactionManager instance
+     */
     @Bean(name = "transactionManager")
-    public HibernateTransactionManager getTransactionManager(
-            SessionFactory sessionFactory) {
-
-        return new HibernateTransactionManager(
-                sessionFactory);
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
     }
 }
--- src/main/java/io/shuritter/spring/config/SpringWebAppInitializer.java
+++ src/main/java/io/shuritter/spring/config/SpringWebAppInitializer.java
@@ -11,21 +11,33 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 /**
+ * Describes the classes, resources, and configuration of the application,
+ * and how the server will use them to execute web requests
  * @author Alexander Nyrkov
  */
-public class SpringWebAppInitializer implements WebApplicationInitializer{
+public class SpringWebAppInitializer implements WebApplicationInitializer {
+
+    /**
+     * Configure the given ServletContext with any servlets, filters, listeners context-params and attributes
+     * necessary for initializing this web application
+     * @param servletContext defines a set of methods that a servlet uses to communicate with its servlet container
+     * @throws ServletException a general exception a servlet can throw when it encounters difficulty
+     */
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
         appContext.register(ApplicationContextConfig.class);
         WebApplicationContext context = getContext();
         servletContext.addListener(new ContextLoaderListener(context));
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
-                "SpringDispatcher", new DispatcherServlet(appContext));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("SpringDispatcher",
+                new DispatcherServlet(appContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/*");
     }
 
+    /**
+     * @return configuration which accepts annotated classes as input
+     */
     private AnnotationConfigWebApplicationContext getContext() {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.setConfigLocation("io.shuritter.spring.config");
