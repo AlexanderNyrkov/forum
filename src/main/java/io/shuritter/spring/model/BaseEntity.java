462dcaedb169079d8cca651dec3062c4a00b9f07
saintshura <alekshurik.nyrkov@gmail.com>
2017-06-11 05:46:08 +0300

    Date instead of DateTime

--- src/main/java/io/shuritter/spring/model/BaseEntity.java
+++ src/main/java/io/shuritter/spring/model/BaseEntity.java
@@ -1,13 +1,8 @@
 package io.shuritter.spring.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.joda.ser.DateTimeSerializer;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
-import org.joda.time.DateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
@@ -15,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+import java.util.Date;
 
 import static javax.persistence.GenerationType.IDENTITY;
 import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME;
@@ -30,44 +26,41 @@ public abstract class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(name = "ID")
-    @Getter @Setter
+    @Getter
+    @Setter
     protected String id;
 
 
     @Column(name = "CREATED_AT")
     @DateTimeFormat(iso = DATE_TIME)
-    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
-    @JsonSerialize(using=DateTimeSerializer.class)
     @Getter
-    protected DateTime createdAt;
+    protected Date createdAt;
 
 
     @Column(name = "UPDATED_AT")
     @DateTimeFormat(iso = DATE_TIME)
-    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
-    @JsonSerialize(using=DateTimeSerializer.class)
-    @Getter @Setter
-    protected DateTime updatedAt;
+    @Getter
+    @Setter
+    protected Date updatedAt;
 
     @Column(name = "IS_DELETED")
-    @Getter @Setter
+    @Getter
+    @Setter
     protected Boolean isDeleted;
 
     /**
      * Default constructor
      */
     public BaseEntity() {
-        this.createdAt = DateTime.now();
-        this.updatedAt = DateTime.now();
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
         this.isDeleted = false;
     }
 
 
     public BaseEntity(Boolean isDeleted) {
-        this.createdAt = DateTime.now();
-        this.updatedAt = DateTime.now();
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
         this.isDeleted = isDeleted;
     }
 }
